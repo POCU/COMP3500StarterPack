98bb7f4883220992be679efdf699c8d1a5b9b9cc
John Choi <john.choi@pocu.academy>
2021-07-03 23:32:00 -0700

    added Point

---
+++ Lab8/src/academy/pocu/comp3500/lab8/maze/Point.java
@@ -0,0 +1,19 @@
+package academy.pocu.comp3500.lab8.maze;
+
+public final class Point {
+    private final int x;
+    private final int y;
+
+    public Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
