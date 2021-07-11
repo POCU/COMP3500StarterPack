bcb3bedb79121417e56c97f9681278d9f44ef671
Chris Yoon <78404624+chrisyoon01@users.noreply.github.com>
2021-07-11 09:37:41 -0700

    Added Task.java to assignment4

---
+++ Assignment4/src/academy/pocu/comp3500/assignment4/project/Task.java
@@ -0,0 +1,38 @@
+package academy.pocu.comp3500.assignment4.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class Task {
+    private final String title;
+    private final ArrayList<Task> predecessors = new ArrayList<>(64);
+    private int estimate;
+
+    public Task(final String title, final int estimate) {
+        this.title = title;
+        this.estimate = estimate;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void addPredecessor(final Task task) {
+        this.predecessors.add(task);
+    }
+
+    public void addPredecessor(final Task... tasks) {
+        for (Task task : tasks) {
+            addPredecessor(task);
+        }
+    }
+
+    public List<Task> getPredecessors() {
+        return Collections.unmodifiableList(this.predecessors);
+    }
+
+    public int getEstimate() {
+        return this.estimate;
+    }
+}
