0452dc5609845b5c85b417506c2ecd2485104322
John Choi <john.choi@pocu.academy>
2021-07-24 20:49:23 -0700

    Lab 11 files

---
+++ Lab11/src/academy/pocu/comp3500/lab11/data/Point.java
@@ -0,0 +1,24 @@
+package academy.pocu.comp3500.lab11.data;
+
+public final class Point {
+    private final int x;
+    private final int y;
+
+    public Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d,%d]", x, y);
+    }
+}
